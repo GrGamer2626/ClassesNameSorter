ad93afc48bc5f2b49c22a6729f01da4f53ee5d10
GrGamer2626 <g2626@tlen.pl>
2024-12-10 16:40:17 +0100

    Add Spring dependencies

--- src/main/java/me/grgamer2626/classesNameSorter/ClassesNameSorterApplication.java
+++ src/main/java/me/grgamer2626/classesNameSorter/ClassesNameSorterApplication.java
@@ -1,8 +1,12 @@
 package me.grgamer2626.classesNameSorter;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class ClassesNameSorterApplication {
 	
 	public static void main(String[] args) {
-	
+		SpringApplication.run(ClassesNameSorterApplication.class, args);
 	}
 }

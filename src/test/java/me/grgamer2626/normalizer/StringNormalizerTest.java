47d8136601314803662ac0b11ac30faf947da828
GrGamer2626 <g2626@tlen.pl>
2024-12-11 01:59:33 +0100

    Add unit tests for StringNormalizer

---
+++ src/test/java/me/grgamer2626/normalizer/StringNormalizerTest.java
@@ -0,0 +1,43 @@
+package me.grgamer2626.normalizer;
+
+import me.grgamer2626.classesNameSorter.application.normalizer.Normalizer;
+import me.grgamer2626.classesNameSorter.application.normalizer.StringNormalizer;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class StringNormalizerTest {
+	private final Normalizer<String> stringNormalizer = new StringNormalizer();
+	
+	@Test
+	public void normalize_inputIsNull() {
+		//given
+		String input = null;
+		
+		//when / then
+		Assertions.assertThrows(IllegalArgumentException.class, () -> stringNormalizer.normalize(input), "Expected IllegalArgumentException when input is null");
+	}
+	
+	@Test
+	public void normalize_inputContainsUpperCaseCharacters() {
+		//given
+		String input = "AaA";
+		
+		//when
+		String result = stringNormalizer.normalize(input);
+	
+		//then
+		Assertions.assertEquals("aaa", result, "All characters should be lowercase.");
+	}
+	
+	@Test
+	public void normalize_inputContainsWhitespaces() {
+		//given
+		String input = " a\ta  a\n ";
+		
+		//when
+		String result = stringNormalizer.normalize(input);
+		
+		//then
+		Assertions.assertEquals("aaa", result, "All whitespaces should be removed.");
+	}
+}

841f8575e87a0737a852441c5d28f9df0af05dd8
GrGamer2626 <g2626@tlen.pl>
2024-12-14 17:32:46 +0100

    Add home page
    Add HTML, CSS and JS for home page.

---
+++ src/main/resources/static/js/REST API Link.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const copyButton = document.getElementById('copyButton');
+    const apiLink = `${window.location.origin}/api/coursesSortedList`;
+
+    copyButton.addEventListener('click', () => {
+        navigator.clipboard.writeText(apiLink)
+            .then(() => alert('Link do API został skopiowany.'))
+            .catch(err => alert('Wystąpił błąd podczas kopiowania linku: ' + err));
+    });
+});
